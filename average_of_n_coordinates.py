cc7a6099f6ee5baa7acddbc7db529dc3754e2305
Aakysank <aakashjs1993@gmail.com>
2023-10-22 21:14:13 +0530

    Comments for reference study

--- average_of_n_coordinates.py
+++ average_of_n_coordinates.py
@@ -1,9 +1,14 @@
 #function that computes centroid
 import itertools
+#this function will get any number of list with different sizes and computes the centroid
 def compute_centroid(*list_of_coords:list)->list:
     if len(list_of_coords) == 0:
         raise BaseException
 
+    #used the itertools module zip_longest function - this function has an argument fillvalue it does the following
+    #if i send two list, [1,2,3] and [1,2,3,4,5] as input, it has different size
+    #zip longest will find out the list with longest length and the list of short lengths has the remaining indices filled with zero
+    # i will be a tuple here in this case.
     centroid = [sum(i)/len(i) for i in itertools.zip_longest(*list_of_coords, fillvalue=0)]
     return centroid
 
